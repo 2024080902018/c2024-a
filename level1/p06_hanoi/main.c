0eaebb9ad9dbfc1cb526628ba0abd21ebe5c032f
2024080902018 <wxy051101a@qq.com>
2024-10-13 16:32:28 +0800

    Update main.c

--- level1/p06_hanoi/main.c
+++ level1/p06_hanoi/main.c
@@ -1,6 +1,24 @@
-#include <printf.h>
+#include<stdio.h>
+#include<stdlib.h>
+void print(char start, char end)
+{
+	printf("%c->%c\n", start, end);
+}
+void move(int n, char start, char end, char temp)
+{
+	if (n == 1) {
+		print(start, end);
+	}
+	else {
+		move(n-1, start, temp, end);
+		print(start, end);
+		move(n-1, temp, end, start);
+	}
+}
 
-int main() {
-    printf("hello world!\n");
-    return 0;
+int main()
+{
+	int n = 64;
+	move(n, 'A', 'B', 'C');
+	return 0;
 }
