bf6d171a9ac5b6c24ca36974cc706cb9e74cc76f
2024080902018 <wxy051101a@qq.com>
2024-10-13 16:01:12 +0800

    Update main.c

--- level1/p02_is_prime/main.c
+++ level1/p02_is_prime/main.c
@@ -1,6 +1,26 @@
-#include <printf.h>
+#include <stdio.h>
+int main()
+{
+	int x;
+	int t = 1;
+	scanf_s("%d", &x);
+	if (x == 1)
+	{
+		t = 0;
+	}
+	else if (x == 2) {
+		t = 1;
+	}
+	else if (x >= 3)
+	{
+		for (int i = 2; i*i < x; i++) 
+		{
+			if (x%i==0) t = 0;
+		}
+		
+	}
+	if (t == 0)printf("不是素数");
+	if (t == 1)printf("是素数");
+	return 0;
 
-int main() {
-    printf("hello world!\n");
-    return 0;
 }
