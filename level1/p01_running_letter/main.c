437a252d5326dc30346ea43b5f2a12de70d40fce
2024080902018 <wxy051101a@qq.com>
2024-10-13 15:59:54 +0800

    Update main.c

--- level1/p01_running_letter/main.c
+++ level1/p01_running_letter/main.c
@@ -1,6 +1,27 @@
-#include <printf.h>
-
+#include <stdio.h>
+#include <windows.h>
+#include<string.h>
+#define width 40
+#define speed 50
 int main() {
-    printf("hello world!\n");
-    return 0;
+	const char letter = 'A';
+	int posi=0;
+	int direc = 1;
+	while (true) {
+		system("cls");
+		int i = 0;
+
+		for (i = 0; i < posi; i++) {
+			printf(" ");
+		}
+		printf("A");
+		posi += direc;
+		if (posi == width - 1||posi==0) {
+			direc *= -1;
+		}
+		
+		Sleep(speed);
+	}
+	system("pause");
+	return 0;
 }
