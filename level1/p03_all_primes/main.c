ba5289178608c744f79a82ff6af59f6d08a4582f
2024080902018 <wxy051101a@qq.com>
2024-10-13 16:02:15 +0800

    Update main.c

--- level1/p03_all_primes/main.c
+++ level1/p03_all_primes/main.c
@@ -1,6 +1,28 @@
-#include <printf.h>
+#include <stdio.h>
+#include<time.h>
+int isPrime(int x) {
+	int t = 1;
+	if (x == 2)t = 1;
+	else if (x >= 3)
+	{
+		for (int j=2; j *j<=x; j++)
+		{
+			if (x % j == 0)t = 0;
+		}
+	}
+	return t;
 
+}
 int main() {
-    printf("hello world!\n");
-    return 0;
+	clock_t start, end;
+	float x;
+	start = clock();
+	for (int i = 2; i <= 1000; i++)
+	{
+		if (isPrime(i) == 1)printf("%d ", i);
+	}
+	end = clock();
+	x = end - start;
+	printf("\n用时：%f", x);
+	return 0;
 }
